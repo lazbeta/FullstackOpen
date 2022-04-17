5af8b093575410234993052ace2626a4adf73866
bet97 <betka.kasanicka@gmail.com>
2022-04-17 13:35:21 +0300

    added router (users and blogs)

--- part5/bloglist-frontend/src/components/Blogs.js
+++ part5/bloglist-frontend/src/components/Blogs.js
@@ -1,24 +1,38 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
+//import { useParams } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 //component
-import Blog from './Blog'
+//import Blog from './Blog'
 
 const Blogs = ({ blogForm }) => {
   const blogs = useSelector(state => state.blogs)
+
+  const blogStyle = {
+    paddingTop: 5,
+    paddingLeft: 20,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5,
+    fontSize: 16
+  }
 
   return (
     <div>
       <h2>Blogs</h2>
       <h4>create new</h4>
       {blogForm()}
-      {blogs
-        .slice().sort((a, b) => b.likes - a.likes)
-        .map((blog) => (
-          <Blog
-            key={blog.id}
-            blog={blog}
-          />
-        ))}
+      <br/>
+      <div>
+
+        {blogs.map((blog) =>
+          <div key={blog.id}  style={blogStyle}>
+            <p><Link to={`/blogs/${blog.id}`}>{blog.title} by {blog.author}</Link></p>
+
+          </div>
+        )}
+
+      </div>
     </div>
   )
 }
